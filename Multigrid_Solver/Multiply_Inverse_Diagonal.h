4ff679a4e9217c25ef219457efb9b5be47e3d549
SoldierDown <dyhard0520@gmail.com>
2020-03-02 20:29:39 -0500

    fixed bugs in Multiply_Inverse_Diagonal

--- Multigrid_Solver/Multiply_Inverse_Diagonal.h
+++ Multigrid_Solver/Multiply_Inverse_Diagonal.h
@@ -17,6 +17,8 @@ class Multiply_Inverse_Diagonal
     using Hierarchy                 = Grid_Hierarchy<Struct_type,T,d>;
     using Allocator_type            = SPGrid::SPGrid_Allocator<Struct_type,d>;
     using Flag_array_mask           = typename Allocator_type::template Array_mask<unsigned>;
+    using Topology_Helper           = Grid_Topology_Helper<Flag_array_mask>;
+    enum {number_of_faces_per_cell  = Topology_Helper::number_of_faces_per_cell};
 
   public:
     Multiply_Inverse_Diagonal(Hierarchy& hierarchy,const std::pair<const uint64_t*,unsigned>& blocks,T Struct_type::* source_channel,
@@ -32,19 +34,41 @@ class Multiply_Inverse_Diagonal
         auto flags=hierarchy.Allocator(level).template Get_Array<Struct_type,unsigned>(&Struct_type::flags);
         auto data_source=hierarchy.Allocator(level).template Get_Const_Array<Struct_type,T>(source_channel);
         auto data_destination=hierarchy.Allocator(level).template Get_Array<Struct_type,T>(destination_channel);
-        
+        uint64_t face_neighbor_offsets[number_of_faces_per_cell];
+        Topology_Helper::Face_Neighbor_Offsets(face_neighbor_offsets);
         const T one_over_dx2=hierarchy.Lattice(level).one_over_dX(0)*hierarchy.Lattice(level).one_over_dX(1);
         const T a=dt*diff_coeff*one_over_dx2; const T coeff1=dt*diff_coeff*(Fc*tau+dt)*one_over_dx2/(dt+tau);
         const T diagonal                = (FICKS)?((T)1.+(T)2.*d*a):((T)1.+(T)2.*d*coeff1);
         const T one_over_diagonal       = (T)1./diagonal;
-        auto multiply_inverse_diagonal=[&](uint64_t offset)
-        {
-            bool to_print=false;
-            for(unsigned e=0;e<Flag_array_mask::elements_per_block;++e,offset+=sizeof(Flags_type))
-                if(flags(offset)&mask) data_destination(offset)=data_source(offset)*one_over_diagonal;
-            
-        };
-        SPGrid_Computations::Run_Parallel_Blocks(blocks,multiply_inverse_diagonal);
+        if(FICKS){
+        	auto ficks_multiply_inverse_diagonal=[&](uint64_t offset)
+          	{
+            	bool to_print=false;
+              	for(unsigned e=0;e<Flag_array_mask::elements_per_block;++e,offset+=sizeof(Flags_type)){
+                  	if(flags(offset)&mask) { T diagonal=(T)1.;
+						for(int face=0;face<number_of_faces_per_cell;++face){
+							uint64_t neighbor_offset=Flag_array_mask::Packed_Add(offset,face_neighbor_offsets[face]);
+							if(hierarchy.template Set<unsigned>(level,&Struct_type::flags).Is_Set(neighbor_offset,Cell_Type_Interior|Cell_Type_Dirichlet)) diagonal+=a;
+						}
+                    	data_destination(offset)=data_source(offset)/diagonal;}}
+          };
+          SPGrid_Computations::Run_Parallel_Blocks(blocks,ficks_multiply_inverse_diagonal);
+        }
+        else{
+        	auto non_ficks_multiply_inverse_diagonal=[&](uint64_t offset)
+          	{
+            	bool to_print=false;
+              	for(unsigned e=0;e<Flag_array_mask::elements_per_block;++e,offset+=sizeof(Flags_type)){
+                  	if(flags(offset)&mask) { T diagonal=(T)1.;
+						for(int face=0;face<number_of_faces_per_cell;++face){
+							uint64_t neighbor_offset=Flag_array_mask::Packed_Add(offset,face_neighbor_offsets[face]);
+							if(hierarchy.template Set<unsigned>(level,&Struct_type::flags).Is_Set(neighbor_offset,Cell_Type_Interior|Cell_Type_Dirichlet)) diagonal+=coeff1;
+						}
+                    	data_destination(offset)=data_source(offset)/diagonal;}}
+          };
+          SPGrid_Computations::Run_Parallel_Blocks(blocks,non_ficks_multiply_inverse_diagonal);
+        }
+
     }
 };
 }
